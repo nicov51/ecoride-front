a5dfce5d54305d067e92fc8a4b44fd92efab98e4
nicov51 <nicolas.viennot@laposte.net>
2025-06-11 22:06:28 +0200

    feature(user-space): create wallet component

--- src/app/features/user-space/pages/shared/wallet/wallet.component.ts
+++ src/app/features/user-space/pages/shared/wallet/wallet.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+
+interface Transaction {
+  type: 'credit' | 'debit';
+  amount: number;
+  reason: string;
+  date: Date;
+}
 
 @Component({
   selector: 'app-wallet',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './wallet.component.html',
   styleUrl: './wallet.component.css'
 })
 export class WalletComponent {
+
+  balance = 20; // Solde initial
+
+  transactions = [
+    {
+      type: 'credit',
+      amount: 20,
+      reason: 'Inscription',
+      date: '15/12/2023'
+    },
+    {
+      type: 'debit',
+      amount: 5,
+      reason: 'Trajet Paris-Lyon',
+      date: '16/12/2023'
+    },
+    {
+      type: 'credit',
+      amount: 10,
+      reason: 'Rechargement',
+      date: '17/12/2023'
+    }
+  ];
+
 
 }
