1c38e3f704779ee49f00ced482a401c31ee1dcb4
nicov51 <nicolas.viennot@laposte.net>
2025-08-21 21:53:20 +0200

    feat(footer): implemantation footer with links email/contactForm

--- src/app/layout/main-layout/footer/footer.component.ts
+++ src/app/layout/main-layout/footer/footer.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon,
+    MatButton,
+    RouterLink
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  manageCookies() {
+    // Logic pour ouvrir le gestionnaire de cookies
+    console.log('Ouvrir gestionnaire cookies');
+  }
 }
